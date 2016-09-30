d8712e457bcf926028da81d72b02f672f9695288
Frederik <frederik290@gmail.com>
2016-10-01 01:09:45 +0200

    factored out the triviality of the data class

--- src/Converter.java
+++ src/Converter.java
@@ -3,57 +3,28 @@
  * Note for reader: coded after midnight ;)
  */
 public class Converter {
-    // The input number split into:
+    private Data data;
     private int ones, tens, hundreds, thousands;
-
-    // Arrays
     private String[] onesArray;
     private String[] tensArray;
     private String[] hundredsArray;
 
     public Converter(){
-        onesArray = new String[9];
-        tensArray = new String[9];
-        hundredsArray = new String[9];
-
-        onesArray[0] = "I";
-        onesArray[1]= "II";
-        onesArray[2] = "III";
-        onesArray[3]= "IV";
-        onesArray[4]="V";
-        onesArray[5]="VI";
-        onesArray[6]="VII";
-        onesArray[7]="VIII";
-        onesArray[8]="IX";
-
-        tensArray[0] = "X";
-        tensArray[1] = "XX";
-        tensArray[2] = "XXX";
-        tensArray[3] = "XL";
-        tensArray[4] = "L";
-        tensArray[5] = "LX";
-        tensArray[6] = "LXX";
-        tensArray[7] = "LXXX";
-        tensArray[8] = "XC";
-
-        hundredsArray[0] = "C";
-        hundredsArray[1] = "CC";
-        hundredsArray[2] = "CCC";
-        hundredsArray[3] = "CD";
-        hundredsArray[4] = "D";
-        hundredsArray[5] = "DC";
-        hundredsArray[6] = "DCC";
-        hundredsArray[7] = "DCCC";
-        hundredsArray[8] = "CM";
+        data = new Data();
+        onesArray = data.getOnesArray();
+        tensArray = data.getTensArray();
+        hundredsArray = data.getHundredsArray();
     }
 
     public String convert(int number){
         String theThousands, theHundreds, theTens, theOnes;
         setConstants(number);
+
         theThousands = makeRomanThousands(thousands);
         theHundreds = makeRomanBelowAThousand(hundredsArray, hundreds);
         theTens = makeRomanBelowAThousand(tensArray, tens);
         theOnes = makeRomanBelowAThousand(onesArray,ones);
+
         return theThousands + theHundreds + theTens + theOnes;
     }
 
